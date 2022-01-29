5d15e7114cff183364a921b53032307d8ac143c9
khafia ayyub <khafiaayyub@gmail.com>
2022-01-29 22:57:30 +0530

    commit

--- script.js
+++ script.js
@@ -1,5 +1,78 @@
 let addBtn = document.querySelector(".add-btn");
-addBtn.addEventListener("click",(e) =>{
-    //display modal
-    //generate ticket
+let removeBtn = document.querySelector(".remove-btn");
+let modalCont = document.querySelector(".modal-cont");
+let mainCont = document.querySelector(".main-cont");
+let textareaCont = document.querySelector(".textarea-cont");
+
+
+let lockClass = "fa-lock";
+let unlockClass = "fa-lock-open";
+
+let ticketsArr = [];
+
+if (localStorage.getItem("jira_tickets")) {
+    // Retrieve and display tickets
+    ticketsArr = JSON.parse(localStorage.getItem("jira_tickets"));
+    ticketsArr.forEach((ticketObj) => {
+        createTicket(ticketObj.ticketColor, ticketObj.ticketTask, ticketObj.ticketID);
+    })
+}
+
+
+
+// Listener for modal priority coloring
+allPriorityColors.forEach((colorElem, idx) => {
+    colorElem.addEventListener("click", (e) => {
+        allPriorityColors.forEach((priorityColorElem, idx) => {
+            priorityColorElem.classList.remove("border");
+        })
+        colorElem.classList.add("border");
+
+        modalPriorityColor = colorElem.classList[0];
+    })
 })
+
+addBtn.addEventListener("click", (e) => {
+    // Display Modal
+    // Generate ticket
+
+    // AddFlag, true -> Modal Display
+    // AddFlag, False -> Modal None
+    addFlag = !addFlag;
+    if (addFlag) {
+        modalCont.style.display = "flex";
+    }
+    else {
+        modalCont.style.display = "none";
+    }
+})
+removeBtn.addEventListener("click", (e) => {
+    removeFlag = !removeFlag;
+    console.log(removeFlag);
+})
+
+
+modalCont.addEventListener("keydown", (e) => {
+    let key = e.key;
+    if (key === "Shift") {
+        createTicket(modalPriorityColor, textareaCont.value);
+        addFlag = false;
+        setModalToDefault();
+    }
+})
+
+function createTicket(ticketColor, ticketTask, ticketID) {
+    let id = ticketID || shortid();
+    let ticketCont = document.createElement("div");
+    ticketCont.setAttribute("class", "ticket-cont");
+    ticketCont.innerHTML = `
+        <div class="ticket-color ${ticketColor}"></div>
+        <div class="ticket-id">#${id}</div>
+        <div class="task-area">${ticketTask}</div>
+        <div class="ticket-lock">
+            <i class="fas fa-lock"></i>
+        </div>
+    `;
+    mainCont.appendChild(ticketCont);
+
+ 
